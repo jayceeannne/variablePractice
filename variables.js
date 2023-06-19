840c688566c9ddee1ece395dc7ea2612009d9c8b
jayceeannne <eric@example.com>
2023-06-19 14:17:50 -0700

    Add more info about me

--- variables.js
+++ variables.js
@@ -4,3 +4,7 @@ heyyy
 var email = "jayceesemail@gmail.com"
 var startingAge = 25
 var doesJayceeLoveSoda = true
+
+var dateOfBirth = 01-03-1998
+var username = "jayceeanne"
+var doesJayceeLoveToTravel = true
